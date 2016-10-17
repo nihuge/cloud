3aa19f85efc0bf9f7c1ec684f97bd7d8a5cdf4bd
nihuge <2944423432@qq.com>
2016-10-18 00:44:16 +0800

    admin

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ function gettrr()
         $res = array();
         foreach ($str as $key => $value) {
             if ($key == 0) {
-
+                echo '';
             } else {
                 foreach ($str[$key] as $key1 => $value1) {
                     if ($res[$key1] == "") {
@@ -55,7 +55,7 @@ function gettrr()
                 }
             }
         }
-        echo htmlspecialchars($st = json_encode((object)$res));
+        echo json_encode((object)$res);
         exit();
     }
 }
@@ -66,20 +66,48 @@ function gethash(){
     $out = curl("http://i.vod.xunlei.com/req_subBT/info_hash/".$seed."/req_num/1000/req_offset/0");
     $dejson = json_decode($out,TRUE);
     if(count($dejson['resp']['subfile_list'])>0){
+        $hashs = array();
+        $num = 0;
         foreach($dejson['resp']['subfile_list'] as $key=>$value){
-            echo "index=".$value['index']."<br>";
-            echo "name=".$value['name']."<br>";
-            echo "file_size=".$value['file_size']."<br>";
-            echo "".$value['']."<br>";
+                $hashs[$num] = array();
+                $hashs[$num]["index"] = $value['index'];
+                $hashs[$num]["name"] = urldecode($value['name']);
+                $hashs[$num]["file_size"] = $value['file_size'];
+                $hashs[$num]["hash"] = $_GET['seed'];
+            $num++;
+//            echo "index=".$value['index']."<br>";
+//            echo "name=".$value['name']."<br>";
+//            echo "file_size=".$value['file_size']."<br>";
+//            echo "".$value['']."<br>";
         }
     }else{
         echo "没有数据";
     }
+    echo json_encode((object)$hashs);
+//    print_r($hashs);
     exit();
 }
+
+
 function getmove(){
     $hash=$_GET['hash'];
     $index=$_GET['index'];
+    $metch = "/<cookie>([\s\S]*?)<\/cookie><\/br><url>([\s\S]*?)<\/url>/";
     $ret=curl("http://aa7761610.s180.cnaaa8.com/2944423432_11_14.php?hash=".$hash."&index=".$index);
+    if($ret<>"﻿无效资源        "){
+    preg_match($metch, $ret, $str1);
+        $returl = array(
+            'cookie' => $str1['1'],
+            'url' => $str1['2']
+        );
+    echo json_encode($returl);
+    }else{
+        echo $ret." hash:".$_GET['hash']." index:".$_GET['index'];
+    }
+}
+function gettrr2(){
+    $metch1 = "/<span class=\"highlight\">(\W+)<\/span>/";
+    $metch2 = "/<a class=\"title\" href=\"\/view\/([A-Za-z0-9]{40})([A-Za-z0-9]{1,9})\">([\s\S]*?)<\/a>/";
+    $res=curl("http://www.shenmidizhi.com/list/".$_GET['word']."-hot-desc-".$_GET['page']);
 }
 ?>
